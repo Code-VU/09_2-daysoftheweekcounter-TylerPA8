b11f5c3b81b3bf7dda49f534d6b16ab61be41d8c
Tyler Paneitz <Tyler.Paneitz@vu.com>
2023-04-05 10:37:11 -0500

    submitted solution

--- counter.py
+++ counter.py
@@ -1,6 +1,17 @@
 def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")
+    days ={}
+    with open(file_name) as file:
+        for line in file:
+            if line.startswith("From "):
+                line = line.strip()
+                line = line.split()
+                days[line[2]] = days.get(line[2], 0) + 1
+    print(days)
+
+
+
 
 
 
